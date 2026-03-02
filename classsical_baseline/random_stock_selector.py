b97347bf2b678674eef9153822196523a6312462
Dao Bui <tdaobui2025@gmail.com>
2026-03-02 00:48:28 -0500

    changed random_stock_selector.py to return a tuple

--- classsical_baseline/random_stock_selector.py
+++ classsical_baseline/random_stock_selector.py
@@ -6,11 +6,12 @@ TODO:
 """
 #MAX Stock is 5
 import random
-def random_stock_selector(stocks: list[str], stocks_data, prev_selected_stocks: list[str]):
+def random_stock_selector(stocks: list[str], stocks_data, prev_selected_stocks: list[str]) -> tuple[str, ...]:
 
     MAX_STOCKS = 5
     MIN_STOCKS = 1
 
+    weightToKeep = 0.0
     if len(prev_selected_stocks) == 1:
         weightToKeep = 0.8
     elif len(prev_selected_stocks) == 2:
@@ -19,26 +20,29 @@ def random_stock_selector(stocks: list[str], stocks_data, prev_selected_stocks: 
         weightToKeep = 0.4
     elif len(prev_selected_stocks) == 4:
         weightToKeep = 0.3
-    elif len(prev_selected_stocks) == 5:
+    elif len(prev_selected_stocks) >= 5:
         weightToKeep = 0.2
 
-    
+    # Convert to lists to allow .remove() in case tuples are passed in,
+    # and to avoid mutating the original passed lists.
+    available_stocks = list(stocks)
+    available_prev = list(prev_selected_stocks)
 
     num_stocks = random.randint(MIN_STOCKS, MAX_STOCKS)
 
     selected_stocks = []
     for n in range(num_stocks):
-        if random.random() < weightToKeep:
-            selected_stocks.append(random.choice(prev_selected_stocks))
-            prev_selected_stocks.remove(selected_stocks[-1])
+        if random.random() < weightToKeep and available_prev:
+            chosen = random.choice(available_prev)
+            selected_stocks.append(chosen)
+            available_prev.remove(chosen)
         else:
-            random_stock = random.choice(stocks)
-            stocks.remove(random_stock) #no duplicates
-            selected_stocks.append(random_stock)
+            if available_stocks:
+                random_stock = random.choice(available_stocks)
+                available_stocks.remove(random_stock) #no duplicates
+                selected_stocks.append(random_stock)
 
-
-    
-    return selected_stocks
+    return tuple(selected_stocks)
 
 
     
